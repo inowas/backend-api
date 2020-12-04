496b07d9ebd68507db0618436621c61641771521
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-12-04 17:03:33 +0100

    Update UsersController.php

--- src/Repository/ToolsRepository.php
+++ src/Repository/ToolsRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Model\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -20,6 +21,19 @@ abstract class ToolsRepository extends ServiceEntityRepository implements ToolRe
             ->andWhere('t.isArchived = :isArchived')
             ->andWhere('t.isScenario = :isScenario')
             ->setParameter('isArchived', false)
+            ->setParameter('isScenario', false)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getAllToolsFromUser(User $user): array
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.user = :user')
+            ->andWhere('t.isArchived = :isArchived')
+            ->andWhere('t.isScenario = :isScenario')
+            ->setParameter('isArchived', false)
+            ->setParameter('user', $user)
             ->setParameter('isScenario', false)
             ->getQuery()
             ->getResult();
