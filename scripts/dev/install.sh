a49e3b6814a2b5aab5b72f8f01016ffae055b839
Ralf Junghanns <ralf@junghanns.it>
2023-07-03 22:32:28 +0200

    Add JWT-Token Generation for dev-environment

--- scripts/dev/install.sh
+++ scripts/dev/install.sh
@@ -8,4 +8,10 @@ docker compose -f docker-compose.dev.yml run php composer install
 docker compose -f docker-compose.dev.yml run php bin/console doctrine:schema:update --force
 docker compose -f docker-compose.dev.yml run php bin/console doctrine:migrations:migrate --no-interaction --query-time --allow-no-migration
 docker compose -f docker-compose.dev.yml run php bin/console app:load-users users.dist.json
+
+JWT_PASSPHRASE=inowas
+ssh-keygen -t rsa -b 4096 -m PEM -P "$JWT_PASSPHRASE" -f $projectRoot/config/jwt/private.pem
+openssl rsa -passin pass:$JWT_PASSPHRASE -in $projectRoot/config/jwt/private.pem -pubout -outform PEM -out $projectRoot/config/jwt/public.pem
+rm $projectRoot/config/jwt/private.pem.pub
+
 $projectRoot/syncSchema.sh
