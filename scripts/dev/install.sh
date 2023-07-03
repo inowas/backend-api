a194064251584c4a6d478ae49208f27a4e05df8c
Christian Rüdiger <christian.ruediger@itexia.com>
2023-07-03 16:17:27 +0200

    add fetching of schema to local dev environment setup

--- scripts/dev/install.sh
+++ scripts/dev/install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+projectRoot=$( cd "$(dirname "${BASH_SOURCE[0]}")/../.." || { echo "determining project root failed"; exit 1; }; pwd -P )
+
 docker network create web
 docker compose -f docker-compose.dev.yml build
 docker compose -f docker-compose.dev.yml run php composer install
 docker compose -f docker-compose.dev.yml run php bin/console doctrine:schema:update --force
 docker compose -f docker-compose.dev.yml run php bin/console doctrine:migrations:migrate --no-interaction --query-time --allow-no-migration
 docker compose -f docker-compose.dev.yml run php bin/console app:load-users users.dist.json
-
+$projectRoot/syncSchema.sh
